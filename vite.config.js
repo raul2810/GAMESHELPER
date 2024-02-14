0eacacbb33edff122ede521bb08be99c12f06a8d
raul2810 <raulde2810@gmail.com>
2024-02-14 10:21:52 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-imporGAMESHELPER/t react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/GAMESHELPER/'
+  base:'/'
 })
